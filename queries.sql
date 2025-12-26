d8d3481f6317feedc71d89777ff0e5a075db498a
Preston Blake <124848277+PrestBlake25@users.noreply.github.com>
2025-12-26 18:12:04 -0500

    Enhance queries with application status counts
    Added a new query to count applications by industry for 'Applied' status.

--- queries.sql
+++ queries.sql
@@ -4,4 +4,18 @@ SELECT COUNT(*) AS total_apps FROM applications;
 SELECT status, COUNT(*) as total
 FROM applications
 GROUP BY status;
+
 --This query groups the applications by their status and counts how many applications are in each status.
+
+With combined_app AS (
+  SELECT *
+  FROM companies
+  JOIN applications
+  ON companies.id = applications.company_id
+)
+SELECT industry, COUNT(*)
+FROM combined_app
+WHERE status = 'Applied'
+GROUP BY industry;
+
+--This query counts the number of applications I have applied to without any response back
